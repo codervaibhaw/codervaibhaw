9489c67d86ed81ef0bf77d50df4f8170a9d41eda
Ankit <Ankit@Ankit-PC>
2021-09-16 22:25:17 +0530

    version 0

---
+++ gitproject/src/gitproject/GitFirst.java
@@ -0,0 +1,12 @@
+package gitproject;
+
+public class GitFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1=new String("Vaibhav Gupta");
+		System.out.println(s1);
+	}
+
+}
+
